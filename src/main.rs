e9b42fc19c50855dc19d70547ef856aec7b15003
ToXiCRising <an12g3@gmx.de>
2022-12-17 22:13:53 +0100

    new tests for level 2 and 3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 use crate::level1::{sv_mult, crossproduct, vv_add, dotproduct, norm};
+use crate::level2::{sm_mult, mv_mult};
+use crate::level3::{mm_mult, frobenius_norm};
+use crate::helper_functions::{print_vector, print_matrix, fill_matrix};
 
 mod level1;
 mod level2;
 mod level3;
+mod helper_functions;
 
 fn main() {
-    println!("Basic implementation of BLAS");
+    println!("/----------------------------\\");
+    println!("|Basic implementation of BLAS|");
+    println!("\\----------------------------/\n");
     let a = 2.0;
     let b = vec![1.0, 2.0, 3.0];
     let c = vec![4.0, 3.0, 5.0];
+    let d = fill_matrix(4,4, "ones");
+    let e = fill_matrix(3,3, "visual");
+    let f = fill_matrix(2, 3, "visual");
+    let g = fill_matrix(3, 4, "visual");
+
     let r1 = sv_mult(a.clone(), b.clone());
     let r2 = vv_add(b.clone(), c.clone());
     let r3 = dotproduct(b.clone(), c.clone());
     let r4 = crossproduct(b.clone(), c.clone());
     let r5 = norm(b.clone());
 
+    let r6 = sm_mult(a.clone(), e.clone());
+    let r7 = mv_mult(e.clone(), b.clone());
+
+    let r8 = mm_mult(e.clone(), r6.clone());
+    let r9 = mm_mult(f.clone(), g.clone());
+    let r10 = frobenius_norm(f.clone()); 
+
+    println!("Tests for level 1");
     println!("a = {a}");
     print!("b = ");
     print_vector(b);
@@ -33,18 +52,35 @@ fn main() {
     print_vector(r4);
 
     println!("Norm of vector b: r5 = {}", r5);
+
+    println!("\nTests for level 2");
+    println!("Matrix d = ");
+    print_matrix(d);
+
+    println!("Matrix e = ");
+    print_matrix(e);
+
+    println!("Matrix e multiplied by scalar a: r6 = ");
+    print_matrix(r6);
+
+    println!("Matrix vector multiplication between e and b: r7 = ");
+    print_vector(r7);
+
+    println!("\nTests for level 3");
+    println!("Matrix multiplication between e and r6: r8 = ");
+    print_matrix(r8);
+
+    println!("Matrix f =");
+    print_matrix(f);
+    println!("Matrix g = ");
+    print_matrix(g);
+    println!("Matrix multiplication between f and g: r9 = ");
+    print_matrix(r9);
+
+    println!("Frobenius norm of matrix f: r10 = {}", r10);
 }
 
-fn print_vector(vector: Vec<f64>){
-    let l = vector.len();
-    print!("v[");
-    for i in 0..l{
-        if i < l-1{
-            print!("{} ", vector[i]);
-        }
-        else{
-            print!("{}", vector[i]);
-        }
-    }
-    println!("]")
-}
+
+
+
+
